5d397ac503f9ede3f9980a6cd1ba79abf6a0b8fb
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2021-02-01 00:00:52 -0300

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { uniqueId } from 'lodash'
+import filesize from 'filesize'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import api from './services/api'
+
+import GlobalStyle from './styles/global'
+
+import { Container, Content } from './styles/App'
+
+import Upload from './components/Upload'
+import FileList from './components/FileList'
+
+class App extends Component {
+	state = {
+		uploadedFiles: []
+	}
+
+	async componentDidMount() {
+		const response = await api.get('/images')
+
+		this.setState({
+			uploadedFiles: response.data.map(image => ({
+				id: image._id,
+				name: image.imageName,
+				readableSize: filesize(image.imageSize),
+				preview: image.imageURL,
+				uploaded: true,
+				url: image.imageURL,
+			}))
+		})
+	}
+	handleUpload = files => {
+		const committedFiles = files.map(file => ({
+			file,
+			id: uniqueId(),
+			name: file.name,
+			readableSize: filesize(file.size),
+			preview: URL.createObjectURL(file),
+			progress: 0,
+			uploaded: false,
+			error: false,
+			url: null,
+		}))
+
+		this.setState({
+			uploadedFiles: this.state.uploadedFiles.concat(committedFiles)
+		})
+
+		committedFiles.forEach(this.processUpload)
+	}
+
+	updateFileProgress = (id, progressData) => {
+		this.setState({ uploadedFiles: this.state.uploadedFiles.map(file => {
+			return id === file.id ? { ...file, ...progressData } : file
+		})})
+	}
+
+	processUpload = (committedFile) => {
+		const data = new FormData()
+
+		data.append('imageFile', committedFile.file, committedFile.name)
+
+		const options = {
+			onUploadProgress: event => {
+				const progress = parseInt(Math.round( (event.loaded * 100) / event.total ))
+
+				this.updateFileProgress(committedFile.id, { progress })
+			}
+		}
+
+		api
+			.post('/images', data, options)
+			.then((response) => {
+				this.updateFileProgress(committedFile.id, {
+					uploaded: true,
+					id: response.data._id,
+					url: response.data.imageURL
+				})
+			})
+			.catch((response) => {
+				this.updateFileProgress(committedFile.id, {
+					error: true
+				})
+			})
+	}
+
+	handleDelete = async (id) => {
+		await api.delete(`/images/${id}`)
+
+		this.setState({
+			uploadedFiles: this.state.uploadedFiles.filter(file => file.id !== id)
+		})
+	}
+
+	componentWillUnmount() {
+		this.state.uploadedFiles.forEach(file => URL.revokeObjectURL(file.url))
+	}
+	render() {
+		const { uploadedFiles } = this.state
+		return (
+				<Container>
+					<Content>
+						<Upload handleUpload={this.handleUpload} />
+						{ !!uploadedFiles.length && (<FileList files={uploadedFiles} handleDelete={this.handleDelete}/>)}
+					</Content>
+					<GlobalStyle />
+				</Container>
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/components/FileList/styles.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+export const Container = styled.ul`
+  margin-top: 20px;
+
+  li {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	align-content: center;
+	color: #444
+
+		& + li {
+			margin-top: 15px;
+		}
+
+		& + div {
+			display: flex;
+			align-items: center;
+			justify-content: flex-end;
+			align-content: baseline;
+
+		}
+
+		a {
+			margin: 5px;
+			align-self: center;
+		}
+  }
+`;
+
+export const FileInfo = styled.div`
+	display: flex;
+	align-items: center;
+	align-content: center;
+
+	div {
+		display: flex;
+		align-content: center;
+		flex-direction: column;
+
+		span {
+			font-size: 12px;
+			color: #999;
+			margin-top: 5px;
+
+			button {
+				border: 0;
+				background: transparent;
+				cursor: pointer;
+				color: #e57878;
+				margin-left: 5px;
+				font-weight: 500;
+			}
+		}
+	}
+`;
+
+export const Preview = styled.div`
+	width: 36px;
+	height: 36px;
+	border-radius: 5px;
+	background-image: url(${props => props.src});
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: 50% 50%;
+	margin-right: 10px;
+`;
